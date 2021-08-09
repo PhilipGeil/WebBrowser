405d4a9af8b7c203a4f21dd8558220106d566be2
PhilipGeil <pgj.philip@gmail.com>
2021-08-09 12:59:50 +0200

    Initial commit

---
+++ WebBrowser/HttpManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBrowser
+{
+    class HttpManager
+    {
+        // Instantiate new instance of HttpClient
+        static readonly HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// Async method for calling get on a website
+        /// </summary>
+        /// <param name="url">url of the website to call</param>
+        /// <returns></returns>
+        public async Task<string> GetWebsite(string url)
+        {
+            // Try/Catch for error handling
+            try
+            {
+                // Create request to given url and await response
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                // return error message
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ WebBrowser/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebBrowser
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Enter the url to get
+            Console.WriteLine("Enter url: ");
+            string url = Console.ReadLine();
+
+            // New instance of class HttpManager
+            HttpManager h = new HttpManager();
+
+            // Call method which gets the website
+            string s = await h.GetWebsite(url);
+
+            // Remove html tags with regex
+            string result = Regex.Replace(s, @"<[^>]*>", string.Empty);
+
+            // Print result
+            Console.WriteLine(result);
+        }
+
+    }
+}
